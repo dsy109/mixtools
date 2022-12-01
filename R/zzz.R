0a5d819077f929b0672be8ffba21971e09c4d3a3
Derek S. Young <54218160+dsy109@users.noreply.github.com>
2022-12-01 11:11:25 -0500

    Update zzz.R
    Added CZI funding mandate.

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,9 @@
   packageStartupMessage(
     paste('mixtools package, version ', info$Version, ', Released ', info$Date, '\n',
           'This package is based upon work supported by the National Science ',
-          'Foundation under Grant No. SES-0518772.\n', sep="")
+          'Foundation under Grant No. SES-0518772 and ', 
+          'the Chan Zuckerberg Initiative: Essential Open Source Software for ',
+          'Science (Grant No. 2020-255193).\n', sep="")
  )
 }
 
